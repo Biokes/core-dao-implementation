a2e303e30326b9d7ecb6b2198d5c66c7d5507039
Biokes <106444765+Biokes@users.noreply.github.com>
2025-07-24 22:37:45 +0100

    [main] (UPDATE): added foundry for sepolia

---
+++ foundry/storage/test/Storage.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test} from "forge-std/Test.sol";
+import {Storage} from "../src/Storage.sol";
+
+contract StorageTest is Test {
+    Storage public counter;
+
+    function setUp() public {
+        counter = new Storage();
+        counter.store(0);
+    }
+
+    function test_Increment() public view {
+        assertEq(counter.retrieve(), 0);
+    }
+
+    function testFuzz_SetNumber(uint256 x) public {
+        counter.store(x);
+        assertEq(counter.retrieve(), x);
+    }
+}
--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -2,21 +2,24 @@ import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const PRIVATE_KEY = vars.get("PRIVATE_KEY")
-const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
-
+const ALCHEMY_PRIVATE_KEY = vars.get("ALCHEMY_PRIVATE_KEY");
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
 const config: HardhatUserConfig = {
- defaultNetwork: "testnet",
+  defaultNetwork: "testnet",
   networks: {
     hardhat: {},
     sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${SEPOLIA_PRIVATE_KEY}`,
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_PRIVATE_KEY}`,
       accounts: [`0x${PRIVATE_KEY}`]
-    }, 
+    },
     testnet: {
       url: "https://rpc.test2.btcs.network",
       accounts: [PRIVATE_KEY],
       chainId: 1114,
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
   },
   solidity: {
     compilers: [

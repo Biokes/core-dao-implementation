152a02ea5da2720d9069c39160ffe8ee4c2b7caf
Biokes <106444765+Biokes@users.noreply.github.com>
2025-07-24 19:48:08 +0100

    [main] (UPDATE): added deplyment script for core dao

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,8 +1,37 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const PRIVATE_KEY = vars.get("PRIVATE_KEY")
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+ defaultNetwork: "testnet",
+
+  networks: {
+    hardhat: {},
+    testnet: {
+      url: "https://rpc.test2.btcs.network",
+      accounts: [PRIVATE_KEY],
+      chainId: 1114,
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.22",
+        settings: {
+          evmVersion: "shanghai",
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  paths: {
+    sources: "./contracts",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
 };
 
 export default config;
